5a2e5850e710aab0d1ef85b5447d52d3aa054ab6
J35HN <jeshua.navaavila@gmail.com>
2026-06-03 17:02:01 -0600

    Created the css file for this component. First classNames defined

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-{/*import css file*/}
+import "../styles/LazyImage.css";
 
 export default function LazyImage({ src, alt, className = ""}) {
     const [loaded, setLoaded] = useSate(false);
