a2a7cb5806ab33da634e58a5d0e6a4be3c408519
J35HN <jeshua.navaavila@gmail.com>
2026-06-03 16:54:35 -0600

    wrote the component jsx

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -2,5 +2,21 @@ import { useState } from "react";
 {/*import css file*/}
 
 export default function LazyImage({ src, alt, className = ""}) {
+    const [loaded, setLoaded] = useSate(false);
 
+    return (
+        <div className = {'lazy-wrapper ${className}'}>
+            {/* The Skeleton only hides on the DOM, so no "jumpiness" of position happens */}
+            <div className = {'lazy-skeleton ${laoded ? "lazy-skeleton--hidden" : ""} '}/>
+
+            {/* Only load image if visible */}
+            <img
+                src = {src}
+                alt = {alt}
+                className = { 'lazy-img ${ loaded ? "lazy-img--visible" : "" }' }
+                onLoad={() => setLoaded(true)}
+                loading="lazy"
+            />
+       </div> 
+    );
 }
