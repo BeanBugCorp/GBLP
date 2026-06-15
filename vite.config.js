763749b0405254053b8cab93b208fc585e19b147
J35HN <jeshua.navaavila@gmail.com>
2026-06-15 13:13:54 -0600

    had to change vite.config to build a preview to verify csp violations. No CSP violations found

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const securityHeaders = {
+  'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+  'X-Content-Type-Options': 'nosniff',
+  'content-security-policy-report-only': "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self' data:; connect-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'"
+};
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-      'X-Content-Type-Options': 'nosniff',
-      'content-security-policy-report-only': "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self' data:; connect-src 'self'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'"
-    }
-  }
+  server: { headers: securityHeaders },
+  preview: { headers: securityHeaders },
 })
