fd668354edf3125a94430fffcda578c0ceea9565
J35HN <jeshua.navaavila@gmail.com>
2026-06-15 17:26:53 -0600

    included the directives for trusted types

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 const securityHeaders = {
   'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
   'X-Content-Type-Options': 'nosniff',
-  'content-security-policy': "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self' https://i.scdn.co https://revistamujeractual.com https://i0.wp.com/ data:; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'"
+  'content-security-policy': "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self' https://i.scdn.co https://revistamujeractual.com https://i0.wp.com/ data:; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; require-trusted-types-for 'script'; trusted-types 'none'"
 };
 
 export default defineConfig({
